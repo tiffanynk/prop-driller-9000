09c69789b68453e689df6940723f112aaf33ec27
LimitToInfinity <21stLimitless@gmail.com>
2020-08-26 11:58:59 -0600

    Adds code to transformers section.

---
+++ src/Containers/TransformersSection.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class TransformersSection extends Component {
+
+  render() {
+    return (
+      <section className = "transformers-section">
+        {/* Transformers here */}
+      </section>
+    );
+  }
+}
+
+export default TransformersSection;
